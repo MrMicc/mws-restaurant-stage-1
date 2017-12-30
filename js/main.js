de206938614bf79e895c49ef2a5d21b173a597d5
Miccieli <luiz.miccieli@gmail.com>
2017-12-30 08:04:37 -0200

    Adjust in CSS, js and html

--- js/main.js
+++ js/main.js
@@ -145,7 +145,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'Image of restaurant '+restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
